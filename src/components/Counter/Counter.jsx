debf7e6cbfd60df496b8e9fcfc6c43c6db7398c9
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-13 21:07:25 +0300

    L-213. Refactor Counter component from course. Add an id in counterChanges so that the correct HistoryItem keeps being highlighted. Not the one in the same position

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -5,6 +5,7 @@ import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
   log(
@@ -43,14 +44,42 @@ const Counter = memo(function Counter({ initialCount }) {
    */
   const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  // const [counter, setCounter] = useState(initialCount);
+  /**
+   * Using key={index} ({@link CounterHistory}) is not recommended. 
+   * It is better to use a unique key for every element in order to bind the functionality to the same element, 
+   *   not the element that happens to be placed in the same position.
+   */
+  const [counterChanges, setCounterChanges] = useState([{
+    id: Math.random() * 1000,
+    value: initialCount
+  }]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
 
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [
+      {
+        id: Math.random() * 1000,
+        value: -1
+      },
+      ...prevCounterChanges
+    ]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [
+      {
+        id: Math.random() * 1000,
+        value: 1
+      },
+      ...prevCounterChanges
+    ]);
   }, []);
 
   return (
@@ -63,11 +92,12 @@ const Counter = memo(function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
 });
