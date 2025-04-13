5e97967726161a282c1c5ea75ea98445c10c59be
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-13 16:03:34 +0300

    L-210. Wrap IconButton with memo. Add useCallback to onClick function

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,6 +1,12 @@
+import { memo } from 'react';
 import { log } from '../../log.js';
 
-export default function IconButton({ children, icon, ...props }) {
+/**
+ * Even if we add memo, IconButton component is re-rendered. 
+ * The reason is that some props are changing. In our case '...props'.
+ * To fix it, we need to add useCallback to the onClick prop in Counter component.
+ */
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
   log('<IconButton /> rendered', 2);
 
   const Icon = icon;
@@ -10,4 +16,6 @@ export default function IconButton({ children, icon, ...props }) {
       <span className="button-text">{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton;
