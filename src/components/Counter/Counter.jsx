abc2f839af8f460e115296a1d80f5ebe47c77c3e
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-13 17:13:49 +0300

    L-211. Add useMemo for prime check function

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback } from 'react';
+import { useState, memo, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -36,7 +36,12 @@ function isPrime(number) {
  */
 const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+
+  /**
+   * useMemo is wrapped around normal functions that are executed in component functions to prevent their execution.
+   * Just like memo, useMemo should be used for functions with complex functionality and not in every function.
+   */
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
   const [counter, setCounter] = useState(initialCount);
 
